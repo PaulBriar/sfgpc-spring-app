5f700ac8f5ae47a195faab71478d6405784cb610
paul.briar <paul.briar@verbinteractive.com>
2020-05-13 19:33:50 -0300

    first commit

---
+++ src/main/java/com/paulbriar/sfgpcspringapp/SfgpcSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.paulbriar.sfgpcspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpcSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgpcSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulbriar/sfgpcspringapp/SfgpcSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulbriar.sfgpcspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgpcSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
