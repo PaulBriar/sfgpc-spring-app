2e0058c3c794e9930a89787cff99bf91aca28edb
paul.briar <paul.briar@verbinteractive.com>
2020-05-19 21:11:20 -0300

    Added BaseEntity class and extended Person, Pet & PetType. Closes #10.

---
+++ clinic-data/src/main/java/com/paulbriar/sfgpcspringapp/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.paulbriar.sfgpcspringapp.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- clinic-data/src/main/java/com/paulbriar/sfgpcspringapp/model/PetType.java
+++ clinic-data/src/main/java/com/paulbriar/sfgpcspringapp/model/PetType.java
@@ -1,6 +1,6 @@
 package com.paulbriar.sfgpcspringapp.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
